ce50c9926d727e9884eedf0fab83fe85282fc217
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-30 15:19:17 +0900

    dynamic time section done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
     }, [])
     return (
         <div className='container'>
-            <h2 className='services-title mt-5'>Our Services: {services.length}</h2>
+            <h2 className='services-title mt-5 text-primary'>Our Services: {services.length}</h2>
             <div className='services-container'>
                 {
                     services.map(service => <Service
