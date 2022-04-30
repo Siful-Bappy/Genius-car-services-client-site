fee8772eb9b02f406435ac205d02c49334c88f3d
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-30 19:52:50 +0900

    login and register button is done

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import "./Register.css";
 
 const Register = () => {
+    const handleRegister = event => {
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(name, email, password);
+        event.preventDefault();
+    }
+
+    const navigate = useNavigate();
+    const navigateLogin = event => {
+        navigate("/login");
+    }
     return (
-        <div>
-            <h2>This is register section</h2>
+        <div className='register-form'>
+            <h2 className='text-center mt-5'>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input  type="text" name="name" id="" placeholder='Your Name' required/>
+                <input  type="email" name="email" id="" placeholder='Email Address' required/>
+                <input  type="password" name="password" id="" placeholder='Your Password' required/>
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account?<Link to={"/login"} onClick={navigateLogin} className="text-danger pe-auto text-decoration-none">Please Register</Link></p>
         </div>
     );
 };
