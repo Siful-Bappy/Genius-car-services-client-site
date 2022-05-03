efcabd20f4cef3f1749f069b4ace09acf2734190
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-03 21:48:44 +0900

    service dynamically added is done

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Navigate, useLocation } from 'react-router-dom';
-import auth from '../../../firebase.init';
-import Loading from '../../Shared/Loading/Loading';
-import { useSendEmailVerification } from 'react-firebase-hooks/auth';
-import { toast, ToastContainer } from 'react-toastify';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+import { useSendEmailVerification } from "react-firebase-hooks/auth";
+import { toast, ToastContainer } from "react-toastify";
 
-const RequireAuth = ({children}) => {
-    const [user, loading] = useAuthState(auth);
-    const location = useLocation();
-    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+const RequireAuth = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+  const location = useLocation();
+  const [sendEmailVerification, sending, error] =
+    useSendEmailVerification(auth);
 
-    // console.log("inside require auth", user);
-    if(loading) {
-        return <Loading></Loading>
-    }
-    // console.log(user);
-    if(!user) {
-        return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-    
-    if(user.providerData[0]?.providerId==='password' && !user.emailVerified) {
-        return (
-            <div className='text-center mt-5'>
-                <h3 className='text-danger'>Your Email is not verified!!!</h3>
-                <h5 className='text-success'>Please verify your email address</h5>
-                <button className='btn btn-primary' onClick={async() => {
-                    await sendEmailVerification();
-                    toast('Sent email');}
-                }>Send verification email Agin</button>
-                <ToastContainer />
-            </div>
-        )
-    }
+  // console.log("inside require auth", user);
+  if (loading) {
+    return <Loading></Loading>;
+  }
+//   console.log(user);
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-    return children;
+  if (user.providerData[0]?.providerId === "password" && !user.emailVerified) {
+    return (
+      <div className="text-center mt-5">
+        <h3 className="text-danger">Your Email is not verified!!!</h3>
+        <h5 className="text-success">Please verify your email address</h5>
+        <button
+          className="btn btn-primary"
+          onClick={async () => {
+            await sendEmailVerification();
+            toast("Sent email");
+          }}
+        >
+          Send verification email Agin
+        </button>
+        <ToastContainer />
+      </div>
+    );
+  }
+
+  return children;
 };
 
 export default RequireAuth;
