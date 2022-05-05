ffd03d4b72c0994d89f8ab4172631899e30ca8b5
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-05 14:17:01 +0900

    services prost operation using exios is done

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Checkout = () => {
     const {serviceId} = useParams();
     const [service] = useServiceDetail(serviceId);
-    const [user, setUser] = useState({
+    const [user] = useAuthState(auth);
+
+    const handlePlaceOrder = event => {
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
+        event.preventDefault();
+    }
+
+    /* if(user) {
+        console.log(user);
+    } */
+
+    /* const [user, setUser] = useState({
         name: "Akber the Great",
         email: "akber@gmail.com",
         address: "Tajmohol road",
@@ -19,18 +38,18 @@ const Checkout = () => {
         const newUser = {address: newAddress, ...rest};
         setUser(newUser);
         console.log(address, rest);
-    }
+    } */
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
-            <form>
-                <input className='w-100 mb-2 ps-1' type="text" name="name" id="" placeholder='Name' required/>
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2 ps-1' type="text" value={user.displayName} name="name" id="" placeholder='Name' readOnly required disabled/>
                 <br />
-                <input className='w-100 mb-2 ps-1' type="email" name="email" id="" placeholder='Email' required/>
+                <input className='w-100 mb-2 ps-1' type="email" name="email" value={user.email} id="" placeholder='Email' readOnly required/>
                 <br />
-                <input className='w-100 mb-2 ps-1' type="text" value={service.name} name="service" id="" placeholder='Service' readOnly/>
+                <input className='w-100 mb-2 ps-1' type="text" value={service.name} name="service" id="" placeholder='Service' readOnly autoComplete='off'/>
                 <br />
-                <input className='w-100 mb-2 ps-1' type="text" name="address" onChange={handleAddressChange} id="" value={user.address} placeholder='Address' required/>
+                <input className='w-100 mb-2 ps-1' type="text" name="address" /* onChange={handleAddressChange} id="" value={user.address} */ placeholder='Address' required/>
                 <br />
                 <input className='w-100 mb-2 ps-1' type="text" name="phone" id="" placeholder='Phone' required/>
                 <br />
